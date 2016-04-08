dd1b22845cda1efee2d5eb7c40671264ac6b8c64
lexria <alexandriamlewis@me.com>
2016-04-08 14:58:12 -0400

    got nav and img on sphome

--- public/spHome/spHome.module.js
+++ public/spHome/spHome.module.js
@@ -1,9 +1,10 @@
 var angular = require('angular');
 var angularRoute = require('angular-route');
 var uiBoot = require('angular-ui-bootstrap');
+// var material = require('angular-material');
 
 angular
-  .module('spHome',['ngRoute', 'ui.bootstrap'])
+  .module('spHome',['ngRoute','ui.bootstrap'/*,'ngMaterial'*/])
   .config(function($routeProvider){
     $routeProvider
     .when('/sphome',{
@@ -11,3 +12,72 @@ angular
       controller: 'SpController as SpCtrl'
     })
   })
+
+
+
+
+// all the material sidenav controllers
+
+
+  // .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  //   $scope.toggleLeft = buildDelayedToggler('left');
+  //   $scope.toggleRight = buildToggler('right');
+  //   $scope.isOpenRight = function(){
+  //     return $mdSidenav('right').isOpen();
+  //   };
+  //   /**
+  //    * Supplies a function that will continue to operate until the
+  //    * time is up.
+  //    */
+  //   function debounce(func, wait, context) {
+  //     var timer;
+  //     return function debounced() {
+  //       var context = $scope,
+  //           args = Array.prototype.slice.call(arguments);
+  //       $timeout.cancel(timer);
+  //       timer = $timeout(function() {
+  //         timer = undefined;
+  //         func.apply(context, args);
+  //       }, wait || 10);
+  //     };
+  //   }
+  //   /**
+  //    * Build handler to open/close a SideNav; when animation finishes
+  //    * report completion in console
+  //    */
+  //   function buildDelayedToggler(navID) {
+  //     return debounce(function() {
+  //       $mdSidenav(navID)
+  //         .toggle()
+  //         .then(function () {
+  //           $log.debug("toggle " + navID + " is done");
+  //         });
+  //     }, 200);
+  //   }
+  //   function buildToggler(navID) {
+  //     return function() {
+  //       $mdSidenav(navID)
+  //         .toggle()
+  //         .then(function () {
+  //           $log.debug("toggle " + navID + " is done");
+  //         });
+  //     }
+  //   }
+  // })
+  //
+  // .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  //   $scope.close = function () {
+  //     $mdSidenav('left').close()
+  //       .then(function () {
+  //         $log.debug("close LEFT is done");
+  //       });
+  //   };
+  // })
+  // .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  //   $scope.close = function () {
+  //     $mdSidenav('right').close()
+  //       .then(function () {
+  //         $log.debug("close RIGHT is done");
+  //       });
+  //   };
+  // });
