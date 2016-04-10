ecc1da3a1cf5d14d44853de3349cd54d34532a74
carolineevail <carolineevail@gmail.com>
2016-04-09 20:17:14 -0400

    Added annotated relationships in entities

--- src/main/java/com/theironyard/entities/Request.java
+++ src/main/java/com/theironyard/entities/Request.java
@@ -1,9 +1,7 @@
 package com.theironyard.entities;//Created by KevinBozic on 4/6/16.
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table (name = "requests")
@@ -13,6 +11,21 @@ public class Request {
     @GeneratedValue
     private int id;
 
+    @OneToMany
+    private List<Task> tasks;
+
     public Request() {
+    }
+
+    public Request(Task task) {
+        this.tasks = tasks;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
