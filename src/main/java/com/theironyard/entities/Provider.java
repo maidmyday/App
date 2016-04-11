7e8654ea8000a9a06e3cdd78623b1d5e9714cad6
KevinBozic <bozic.kevin@gmail.com>
2016-04-11 12:18:08 -0400

    Fixed login client route

--- src/main/java/com/theironyard/entities/Provider.java
+++ src/main/java/com/theironyard/entities/Provider.java
@@ -79,4 +79,11 @@ public class Provider {
         this.phoneNumber = phoneNumber;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
