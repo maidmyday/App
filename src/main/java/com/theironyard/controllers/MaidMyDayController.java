4ecda970ddceda90c6e162a896a01518c69a28cc
KevinBozic <bozic.kevin@gmail.com>
2016-04-13 15:33:43 -0400

    Messing around with file upload. Not functional. Main app still works

--- src/main/java/com/theironyard/controllers/MaidMyDayController.java
+++ src/main/java/com/theironyard/controllers/MaidMyDayController.java
@@ -15,6 +15,21 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
 /**
  * Created by Caroline on 4/5/16.
  */
@@ -309,5 +324,65 @@ public class MaidMyDayController {
     @RequestMapping(path = "/task", method = RequestMethod.GET)
     public Task populateTasks() {
         return null;
+    }
+
+
+
+
+    //////
+    ////// Messing with file upload
+    //////
+    ////// Credit : http://www.journaldev.com/2573/spring-mvc-file-upload-example-tutorial-single-and-multiple-files
+    //////
+    ////// Need to figure out how to ONLY allow jpeg, png, etc... only photos. No mp4, mp3, mov, etc...
+    ////// We did this in class
+    //////
+    ////// Also need to set a max file size limit
+    ////// We've also done this in class, but it might be accomplished on the frontend
+    //////
+
+
+
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(MaidMyDayController.class);
+
+    /**
+     * Upload single file using Spring Controller
+     */
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("name") String name,
+                             @RequestParam("file") MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                logger.info("Server File Location="
+                        + serverFile.getAbsolutePath());
+
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
     }
 }
