0873cf25fe315f7b8dc6f9fad6c2086703713efb
carolineevail <carolineevail@gmail.com>
2016-04-13 19:17:17 -0400

    Worked on GET /notification and PUT /request/{id}

---
+++ src/main/java/com/theironyard/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.theironyard.utils;
+
+/**
+ * Created by Caroline on 4/13/16.
+ */
+public class Constants {
+
+    public static final String NOTIFICAITON_TEXT = "Your provider has completed your request!";
+}
